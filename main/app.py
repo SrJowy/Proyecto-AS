87f9fc9947b1ba889a740a3c2a0933f28d0cefe8
Joel <joelbraortiz@gmail.com>
2022-11-15 18:47:02 +0100

    Aplicacion cliente

--- main/app.py
+++ main/app.py
@@ -4,15 +4,29 @@ import time
 connection = client.connect("http://localhost:4200/", username="crate")
 
 cursor = connection.cursor()
+cursor.execute("select table_name from information_schema.tables WHERE table_name = 'people' limit 100")
+exists = cursor.fetchone()
+if (not exists):
+    cursor.execute("""CREATE TABLE people ( name text, date text, country text)""")
 
-#cursor.execute("""CREATE TABLE people ( name text, date text, country text, age integer)""")
-
-cursor.execute("""INSERT INTO people (name, date, country, age)
-            VALUES (?, ?, ?, ?)""",("Joel Bra", "09/07/2001", "España", 21))
-
-cursor.execute("DELETE FROM people WHERE country = ?", ('España',))
-time.sleep(5)
-#cursor.execute("UPDATE people set country = 'Españita' WHERE NAME = 'Joel Bra'")
-cursor.execute("SELECT * FROM people WHERE name = 'Joel Bra'")
-res = cursor.fetchone()
-print(res)
+while True:
+    print("1. Introducir datos\n2. Borrar datos\n3. Ver datos\n4. Salir")
+    option = input("Elige una opción: ")
+    if option == "1":
+        name = input("Escribe tu nombre: ")
+        date = input ("Escribe tu fecha de nacimiento: ")
+        country = input("Escribe tu país: ")
+        cursor.execute("""INSERT INTO people (name, date, country)
+            VALUES (?, ?, ?)""",(name, date, country))
+        print("Se ha añadido la entrada correctamente\n")
+    elif option == "2":
+        name = input("Escribe el nombre de la entrada que quieres borrar: ")
+        cursor.execute("DELETE FROM people WHERE name = ?", (name,))
+        print("Se ha borrado la entrada correctamente\n")
+    elif option == "3":
+        name = input("Escribe el nombre de la entrada que quieres consultar: ")
+        cursor.execute("SELECT * FROM people WHERE name = ?", (name,))
+        res = cursor.fetchone()
+        print(res + "\n")
+    elif option == "4":
+        exit(0)
