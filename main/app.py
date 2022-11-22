bf61803177612d39c9f8485c592c249b4207b838
Joel <joelbraortiz@gmail.com>
2022-11-22 18:48:56 +0100

    Frontend terminado

--- main/app.py
+++ main/app.py
@@ -7,16 +7,35 @@ CORS(app)
 
 @app.route('/retrievedata',methods=['GET'])
 def retrieve_data():
-    #nombre = request.args.get('nombre')
-    response = jsonify({'mError':'error'})
-    return response
+    connection = client.connect("http://172.20.0.2:4200/", username="crate")
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute("select table_name from information_schema.tables WHERE table_name = 'tasks' limit 100")
+        exists = cursor.fetchone()
+        if (not exists):
+            cursor.execute("""CREATE TABLE tasks (name text, task text, date text)""")
+        cursor.execute("SELECT * FROM tasks")
+        tasks = cursor.fetchall()
+    connection.close()
+    retorno = {"lTareas": []}
+    for t in tasks:
+        retorno["lTareas"].append({"name": t[0], "task": t[1], "date": t[2]})
+    
+    return jsonify(retorno)
 
 @app.route('/newentry', methods=['POST'])
 def new_entry():
     name = request.form['name']
     task = request.form['task']
     date = request.form['date']
-    return str(name) + str(task) + str(date)
+
+    connection = client.connect("http://172.20.0.2:4200/", username="crate")
+    cursor = connection.cursor()
+    cursor.execute("""INSERT INTO "doc"."tasks" (name, task, date) 
+                   VALUES (?, ?, ?)""",(name, task, date))
+    connection.close()
+
+    return "Succesfull"
 
 app.run(host='0.0.0.0', port=4201)
 
--- main/public/main.js
+++ main/public/main.js
@@ -6,8 +6,22 @@ const retrieveData = () => {
     xmlHttp.send();
 
     xmlHttp.onreadystatechange = (e) => {
-        console.log(xmlHttp.responseText)
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            let json = JSON.parse(xmlHttp.responseText)
+            let div = document.getElementById("data")
+            for (i = 0; i< json.lTareas.length; i++) {
+                agregarDatos(json.lTareas[i].name, json.lTareas[i].task, json.lTareas[i].date)
+            }
+        }
     }
+}
+
+const agregarDatos = (name, task, date) => {
+    div = document.getElementById("data")
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(name + ", " + task+ ", " + date);
+    node.appendChild(textnode)
+    div.appendChild(node)
 }
 
 const newEntry = () => {
@@ -18,12 +32,14 @@ const newEntry = () => {
     if (name != "" && task != "" && date != "") {
         const request = new XMLHttpRequest();
         const url = 'http://localhost:4201/newentry';
-        var params = 
         request.open("POST", url, true);
         request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
         request.onreadystatechange = (e) => {
-            console.log(request.responseText)
+            if (request.readyState == 4 && request.status == 200) {
+                agregarDatos(name, task, date)
+                alert("Success")
+            }
         }
 
         request.send("name="+name+"&task="+task+"&date="+date)
